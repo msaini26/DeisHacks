ee872bb07caa2b0b1e324ac606c2aa0ee886e8c7
Mansi S <mansisaini26@gmail.com>
2021-03-20 22:54:44 +0000

    updated server.js endpoint

--- payment/server.js
+++ payment/server.js
@@ -1,9 +1,9 @@
-const stripe = require('stripe')('sk_test_51IXAcIE2MMOUDocrQSxEfURlxG8q4wmXf9K1TuhhybfGQgCPlqYVhwOlj0U790biz3CuiMXWu7NT6vEETGg9qH9B00JbayZc4N');
+// This example sets up an endpoint using the Express framework.
+// Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
+
 const express = require('express');
 const app = express();
-app.use(express.static('.'));
-
-const YOUR_DOMAIN = 'http://localhost:4242';
+const stripe = require('stripe')('sk_test_51IXAcIE2MMOUDocrQSxEfURlxG8q4wmXf9K1TuhhybfGQgCPlqYVhwOlj0U790biz3CuiMXWu7NT6vEETGg9qH9B00JbayZc4N')
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
@@ -13,8 +13,7 @@ app.post('/create-checkout-session', async (req, res) => {
         price_data: {
           currency: 'usd',
           product_data: {
-            name: 'Stubborn Attachments',
-            images: ['https://i.imgur.com/EHyR2nP.png'],
+            name: 'T-shirt',
           },
           unit_amount: 2000,
         },
@@ -22,11 +21,11 @@ app.post('/create-checkout-session', async (req, res) => {
       },
     ],
     mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    success_url: 'https://deishacks.itzsims.repl.co/payment/success.html',
+    cancel_url: 'https://deishacks.itzsims.repl.co/payment/cancel.html',
   });
 
   res.json({ id: session.id });
 });
 
-app.listen(4242, () => console.log('Running on port 4242'));
+app.listen(4242, () => console.log(`Listening on port ${4242}!`));
